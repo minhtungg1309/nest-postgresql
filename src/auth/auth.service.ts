441a96207ed5bc60d1dd9180d09debfeda16c1a7
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-08-05 13:56:01 +0700

    video step 15

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,23 +14,22 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
+    if (!user) return null;
+
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!user || !isValidPassword) return null;
+    if (!isValidPassword) return null;
     return user;
   }
 
   async login(user: any) {
     const payload = { username: user.email, sub: user._id };
     return {
-      data: {
-        user: {
-          email: user.email,
-          _id: user._id,
-          name: user.name
-        },
-        access_token: this.jwtService.sign(payload),
-      }
-
+      user: {
+        email: user.email,
+        _id: user._id,
+        name: user.name
+      },
+      access_token: this.jwtService.sign(payload),
     };
   }
 
