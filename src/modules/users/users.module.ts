20d7bda73b27cae1e2eb83dfd451fd695cff0fb7
minhtungg1309 <tungho.mth@gmail.com>
2025-12-05 08:07:19 +0700

    s

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { ElasticsearchModule } from '@/elasticsearch/elasticsearch.module';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
-  imports: [ElasticsearchModule],
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule { }
